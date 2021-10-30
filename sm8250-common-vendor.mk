2dbbfde7d8604a9bcf572392e62a0fbc85872a41
goodmeow <harunbam3@gmail.com>
2021-10-30 19:28:36 +0200

    sm8250-common: move wfd to vendor-extra

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -900,7 +900,6 @@ PRODUCT_PACKAGES += \
     WfdService \
     ims \
     qcrilmsgtunnel \
-    WfdCommon \
     android.hardware.gnss@2.1-service-qti \
     android.hardware.neuralnetworks@1.3-service-qti-hta \
     android.hardware.neuralnetworks@1.3-service-qti \
